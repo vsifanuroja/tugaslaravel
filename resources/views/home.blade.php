2e97b41d73d51c82fd8cb501a3b94011d597175b
ara sky <racarnation12@gmail.com>
2024-09-26 14:54:46 +0700

    menambahkan file

---
+++ resources/views/home.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QUEEN</title>
+    <style>
+        body {
+            background-color: pink;
+            font-family: 'Arial', sans-serif;
+        }
+
+        #arunika {
+            color: #ff69b4;
+            text-align: center;
+            margin-top: 50px;
+        }
+
+        .nayanika {
+            color: #fff;
+            font-size: 20px;
+            background-color: #d692b6;
+            border-radius: 10px;
+            padding: 20px;
+            width: 60%;
+            margin: 20px auto;
+            text-align: center;
+        }
+
+        a[target] {
+            color: #edaecd;
+        }
+
+    </style>
+</head>
+
+<body>
+
+    <h1 id="arunika">WELCOME TO MY WORLD</h1>
+
+    <div class="nayanika">
+        <p>You can either experience the pain of discipline or the pain of regret. The choice is yours</p>
+        <p>Dalam senja yang sunyi, temukan keindahan yang tak terduga dalam keheningan yang mendalam. <a
+                href="https://www.instagram.com/racarnation" target="_blank">racarnation.com</a>
+        </p>
+    </div>
+
+</body>
+
+</html>
